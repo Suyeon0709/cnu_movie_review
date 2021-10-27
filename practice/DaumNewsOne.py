0c90b5682cf9c98badab50676a7ccb5250714e85
main_kiosk.py <rlatndus173@naver.com>
2021-10-28 00:28:17 +0900

    DAUM News 목록 -> 제목, 본문 수집

---
+++ practice/DaumNewsOne.py
@@ -0,0 +1,33 @@
+# 웹 크롤링 => 네이버 뉴스
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# requests => 웹사이트 코드 복사 GET
+# BeautifulSoup4 => requests GET 해온 코드에서 필요한 정보만 find해서 가져오기
+
+url = 'https://news.v.daum.net/v/20211021152915953'
+result = requests.get(url)
+# print(result.text)
+
+# <tag id of class> </tag>
+# class => .
+# id => #
+
+# title = doc.select('h3.tit_view') # list type
+# title2 = doc.select('h3.tit_view')[0]
+
+doc = BeautifulSoup(result.text, 'html.parser')
+title = doc.select('h3.tit_view')[0].get_text()
+contents = doc.select('section p')
+
+contents.pop(-1) # 기자 정보 삭제
+content = '' # 본문 총합
+for info in contents:
+    content += info.get_text()
+
+print('###########################################')
+print('#뉴스 제목: {}'.format(title))
+print('###########################################')
+print('# 뉴스 본문: {}'.format(content))
